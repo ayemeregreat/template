0f235305b3cc8a1d8e3bd38d3965758420d41406
Osagie Great Ayemere <mrosagiegreat@gmail.com>
2026-06-27 23:26:11 +0100

    Route ts local host 3002

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -26,7 +26,7 @@ export async function GET() {
       );
     }
 
-    const redirectUri = redirect_uris[0] ?? "urn:ietf:wg:oauth:2.0:oob";
+    const redirectUri = redirect_uris[0] ?? "http://localhost:3002/api/auth/callback";
 
     const oauth2Client = new google.auth.OAuth2(
       client_id,
